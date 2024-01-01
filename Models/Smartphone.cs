e15c2a01539919c51894a128fcff2fe6b45bff51
Sanderson Rito <sanderson_rito_15@hotmail.com>
2024-01-01 18:24:11 -0300

    Desafio Trilha POO DotNet Concluídogit add trilha-net-poo-desafio.sln

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string IMEI;
+        public int Memoria;
+        // Implementar as propriedades faltantes de acordo com o diagrama - TODO implementado!
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // Passar os parâmetros do construtor para as propriedades  - TODO implementado!
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizar os testes com as classes Nokia e Iphone  - TODO implementado!
+
+Console.WriteLine("---------- Testes Smartphone Nokia ---------- \n");
+Smartphone nokia = new Nokia("(99)99999-9999", "Nokia 3310", "999999999999999", 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n\n");
+
+Console.WriteLine("---------- Testes Smartphone iPhone ---------- \n");
+Smartphone iphone = new Iphone("(77)77777-7777", "iPhone 3GS", "777777777777777", 32);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
